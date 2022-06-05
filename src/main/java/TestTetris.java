eb5bc60039ccc03e8650911bcc33e32aa8f1d1ab
Panda <2224251265@qq.com>
2022-06-05 15:48:22 +0800

    Tetris
    Tetris bug fixed

--- src/main/java/TestTetris.java
+++ src/main/java/TestTetris.java
@@ -1,6 +1,10 @@
+import java.util.Random;
+
 public class TestTetris {
     public static void main(String[] args){
-        Tetris tetris = new Tetris(20, 10, 2);
+        Tetris tetris = new Tetris(10, 10, 2);
+        int cnt = 0;
+        Random random = new Random();
         new Thread(tetris).start();
         while(true){
             try{
@@ -11,7 +15,16 @@ public class TestTetris {
             if(tetris.hasFrame() == false) continue;
             Frame frame = tetris.getFrame();
             if(frame.state == Tetris.fail) break;
+            cnt++;
+            if(cnt != 25) continue;
+            cnt = 0;
             System.out.println("Nexts :" + frame.nexts);
+            for(int i = 9; i >= 0; i--){
+                System.out.println(frame.scene.substring(i*10, i*10+10));
+            }
+            System.out.println();
+            tetris.left(0);
+            tetris.left(1);
         }
     }
 }
